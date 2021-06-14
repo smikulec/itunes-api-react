768c7e013387d2e43c6208877903645eb07bfcdb
Sonja Mikulec <sonja.mikulec@gmail.com>
2021-06-14 17:52:57 +0200

    Updates

---
+++ precache-manifest.b618d6541127c766718f707d5b682c1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77763729d5b350e9d91cf741eff9b648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f95c2a17c2cfc991b6e3",
+    "url": "/static/css/main.56d58ace.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "f95c2a17c2cfc991b6e3",
+    "url": "/static/js/main.2a9620a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
